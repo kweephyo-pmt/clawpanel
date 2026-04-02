783260af84519b3024d21be329390550bd5cd539
Phyo_Min_Thein <6531503172@lamduan.mfu.ac.th>
2026-04-02 14:50:18 +0700

    fix: explicit allowedDevOrigins for server HMR

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Allow Hot Module Replacement (HMR) to work via your remote server IP
+  allowedDevOrigins: ['72.62.253.53'],
 };
 
 export default nextConfig;
